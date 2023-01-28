b54ed0d482393e3d7137997e3ae16512c7ffd6fb
StructuralGio <122745788+StructuralGio@users.noreply.github.com>
2023-01-28 15:42:55 +0100

    1

--- main.cpp
+++ main.cpp
@@ -1,13 +1,38 @@
 // c++ hello world program
 
 #include <iostream>
-
+using namespace std;
 int main() {
+    int inputvalue, inputvalue2;
+    cout << "Welcome to ProMan" << endl << endl;
+    cout << "What do you want to do?" << endl;
+    cout << "1 Team management" << endl;
+    cout << "2 Project management" << endl;
 
-    // Program code here
+    cin >> inputvalue;
 
-    std::cout << "Hello world!";
-    system("pause");
+    if (inputvalue == 1 ) {
+        cout << "Team management" << endl << endl;
+        cout << "1 Add TeamMate" << endl;
+        cout << "2 Modify TeamMate" << endl;
+        cout << "3 Remove TeamMate" << endl;
+        cout << "4 Show TeamMate list" << endl;
+        cout << "5 Go back" << endl;
+    }
+    else if (inputvalue == 2) {
+        cout << "Project management" << endl << endl;
+        cout << "1 Add Project" << endl;
+        cout << "2 Modify Project" << endl;
+        cout << "3 Remove Project" << endl;
+        cout << "4 Show Project list" << endl;
+        cout << "5 Go back" << endl;
+    }
+
+    cin >> inputvalue2;
+
+    cout << "Selection" << inputvalue << "," << inputvalue2;
+
+
     return 0;
 
 }
